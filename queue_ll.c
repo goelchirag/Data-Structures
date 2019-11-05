7010682fd5d2e568a8561ba66b9d4de9f9b81bf4
goelchirag <55796593+goelchirag@users.noreply.github.com>
2019-11-05 15:12:59 +0530

    Create queue_ll.c

---
+++ queue_ll.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+int n,count=0;
+struct node
+{
+   int data;
+   struct node *link;
+
+}*root=NULL,*temp=NULL,*p;
+
+void insert()
+{
+
+	temp=(struct node *)malloc(sizeof(struct node));
+
+	printf("\nenter the no. ");
+	scanf("%d",&temp->data);
+	temp->link=NULL;
+
+		if(root==NULL)
+		{
+
+			root=temp;
+	
+		}
+		else
+		{
+			 p=root;
+				while(p->link!=NULL)
+				   p=p->link;  
+		p->link=temp;
+		}
+	count++;
+	}
+
+void display()
+{
+
+p=root;
+
+  if(p==NULL)
+  {
+         printf("\nTHE LINKED LIST ID EMPTY\n");
+  }
+  else
+  {
+	while(p!=NULL)
+	{
+	
+	   printf("%d -->",p->data);
+	   p=p->link;
+
+	}
+
+  }
+}
+void delete(){
+	if(count==0)
+		printf("\n The list is empty");
+	else{ 
+	temp=root;
+	root=root->link;
+	printf("\n Deleted %d",temp->data);
+	free(temp);count--;
+	}
+	
+}
+
+	
+int main()
+{
+
+int ch;
+	while(1)
+	{
+	printf("\n\nPLEASE ENTER YOUR CHOICE: \n1.INSERT\n2.DISPLAY\n3.DELETE\n4.EXIT");
+	scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:
+				insert();
+				break;
+			case 2:
+				display();
+				break;
+		      
+			case 3:
+				delete();
+				break;
+			default:
+				printf("\n\nTHANK YOU\n\n");
+ 				return 0;
+
+		}
+
+
+	}
+
+}
